b2b4e6fe9eb1aecda4b074b65c4144945dcb3fbc
Joji Shiotsuki <149657863+jojiShiotsuki@users.noreply.github.com>
2024-08-10 17:06:18 +0800

    Added operators

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@ import { RouterLink } from 'vue-router';
 <template>
 	<section class="intro-section">
         <h1>Welcome to The Calculator App</h1>
-        <p>My calculator app is designed to help you perform a variety of calculations easily and efficiently. Whether you're doing basic arithmetic or more complex operations, our user-friendly interface makes it simple.</p>
+        <p>My simple calculator App</p>
         <RouterLink to="/calculator" class="btn-primary">Go to Calculator</RouterLink>
 	</section>
 </template>
--- src/views/CalculatorView.vue
+++ src/views/CalculatorView.vue
@@ -1,16 +1,47 @@
 <script setup>
 import { ref } from 'vue'
 
-const total = ref(0)
-const num = ref('')
+const display = ref(0)
+const currentNum = ref('')
+const previousNum = ref('')
+const operator = ref('')
+
 const getNum = (event) => {
 	const number = event.target.getAttribute('data-number');
-	num.value = number
-	total.value = total.value * 10 + parseInt(number)
-	console.log(total.value)
+	currentNum.value += number
+	display.value = currentNum.value
 }
-const addition = (event) => {
-	
+
+const setOperator = (op) => {
+	if(currentNum.value){
+		previousNum.value = currentNum.value;
+		currentNum.value = '';
+	}
+	operator.value = op;
+}
+
+const calculate = () => {
+	let result = 0;
+	if(operator.value === '+'){
+		result = parseFloat(previousNum.value) + parseFloat(currentNum.value)
+	}else if(operator.value === '-'){
+		result = parseFloat(previousNum.value) - parseFloat(currentNum.value)
+	}else if(operator.value === '*'){
+		result = parseFloat(previousNum.value) * parseFloat(currentNum.value)
+	}else {
+		result = parseFloat(previousNum.value) / parseFloat(currentNum.value)
+	}
+	display.value = result;
+	currentNum.value = result.toString(); // Store the result as the current number for further calculations
+  	previousNum.value = ''; // Reset previous number
+ 	operator.value = ''; // Reset operator
+}
+
+const clear = () => {
+	currentNum.value = '';
+ 	previousNum.value = '';
+  	operator.value = '';
+  	display.value = 0; // Reset the display
 }
 
 </script>
@@ -18,25 +49,24 @@ const addition = (event) => {
 <template>
 	<section class="calculatorContainer">
 		<div class="calculator">
-			<div class="display" id="display">{{ total }}</div>
+			<div class="display" id="display">{{ display }}</div>
 			<div class="buttons">
-				<button class="btn operator" data-operator="C">C</button>
-				<button class="btn operator" data-operator="/">/</button>
-				<button class="btn operator" data-operator="*">*</button>
-				<button class="btn operator" data-operator="-">-</button>
+				<button @click="clear" class="btn operator" data-operator="C">C</button>
+				<button @click="setOperator('/')" class="btn operator" data-operator="/">/</button>
+				<button @click="setOperator('*')" class="btn operator" data-operator="*">*</button>
+				<button @click="setOperator('-')" class="btn operator" data-operator="-">-</button>
 				<button @click="getNum" class="btn" data-number="7">7</button>
 				<button @click="getNum" class="btn" data-number="8">8</button>
 				<button @click="getNum" class="btn" data-number="9">9</button>
-				<button @click="addition" class="btn operator" data-operator="+">+</button>
+				<button @click="setOperator('+')" class="btn operator" data-operator="+">+</button>
 				<button @click="getNum" class="btn" data-number="4">4</button>
 				<button @click="getNum" class="btn" data-number="5">5</button>
 				<button @click="getNum" class="btn" data-number="6">6</button>
-				<button class="btn operator" data-operator="=">=</button>
+				<button @click="calculate" class="btn operator" data-operator="=">=</button>
 				<button @click="getNum" class="btn" data-number="1">1</button>
 				<button @click="getNum" class="btn" data-number="2">2</button>
 				<button @click="getNum" class="btn" data-number="3">3</button>
 				<button @click="getNum" class="btn" data-number="0">0</button>
-				<button class="btn operator" data-operator=".">.</button>
 			</div>
     	</div>
 	</section>
